95b24901458bfb3d39997b01b1287f66127e1b28
aliasaad22 <ali.asaad1@student.ehb.be>
2026-05-07 14:04:32 +0200

    TEAMS INDEX pagina maken en invogen

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
 Route::get('/news', [NewsController::class, 'index']);
 Route::get('/news/{id}', [NewsController::class, 'show']);
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/news/{id}', [NewsController::class, 'show']);
+Route::get('/teams', [TeamController::class, 'index']);
 
 
 require __DIR__.'/settings.php';
