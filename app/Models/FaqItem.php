1aba50e7e80ba2c3aa58bf96009c90a296b06b83
aliasaad22 <ali.asaad1@student.ehb.be>
2026-05-13 14:24:17 +0200

    faqietm toevogen

--- app/Models/FaqItem.php
+++ app/Models/FaqItem.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class FaqItem extends Model
 {
-    //
+    protected $fillable = ['question', 'answer', 'faq_category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(FaqCategory::class);
+    }
 }
+
